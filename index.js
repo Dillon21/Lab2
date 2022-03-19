d9aee16a8eefb8919b2c652e92c681f252095cef
Dillon21 <dillon9821@gmail.com>
2022-03-19 20:41:22 +0000

    change port code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var http = require("http");
-const PORT = 8080
+
 http.createServer(function (request, response) {
 	response.writeHead(200,{'Content-Type' : 'text/plain'});
 	
 	response.end('Hello World\n');
-	
-}).listen(PORT);
+	const PORT = process.env.PORT || 3000;
+a	pp.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+})
 
 console.log('Server running at http://127.0.0.1:8080/')
 
