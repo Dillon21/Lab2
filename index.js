5b0292da32d2597e3d5bcef5be25235e6024287f
Dillon21 <dillon9821@gmail.com>
2022-03-19 20:22:46 +0000

    heroku first try

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require("http");
+const PORT = 8080
 http.createServer(function (request, response) {
 	response.writeHead(200,{'Content-Type' : 'text/plain'});
 	
 	response.end('Hello World\n');
-}).listen(8080);
+	
+}).listen(PORT);
 
 console.log('Server running at http://127.0.0.1:8080/')
 
